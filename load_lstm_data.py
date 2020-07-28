20f7a5babe4a8bc3b1be474044b7ce83bc7689a0
jmframe <jmframe@crimson.ua.edu>
2020-07-28 01:09:38 -0400

    first working modification

--- load_lstm_data.py
+++ load_lstm_data.py
@@ -3,6 +3,19 @@ import pandas as pd
 import numpy as np
 import pickle as pkl
 
+def split_test_train(data_dict, year):
+    obs_drop = ['year','day','hour', 'x1', 'x2', 'p']
+    data_dict['obs_test'] = data_dict['obs'].loc[data_dict['obs']['year'] >= year-1]
+    data_dict['obs_train'] = data_dict['obs'].loc[data_dict['obs']['year'] < year-1]
+    data_dict['obs_test'] = data_dict['obs_test'].drop(obs_drop, axis=1)
+    data_dict['obs_train'] = data_dict['obs_train'].drop(obs_drop, axis=1)
+    forcing_drop = ['year','day','hour', 'dummy']
+    data_dict['forcing_test'] = data_dict['forcing'].loc[data_dict['obs']['year'] >= year-1]
+    data_dict['forcing_train'] = data_dict['forcing'].loc[data_dict['obs']['year'] < year-1]
+    data_dict['forcing_test'] = data_dict['forcing_test'].drop(forcing_drop, axis=1)
+    data_dict['forcing_train'] = data_dict['forcing_train'].drop(forcing_drop, axis=1)
+    return data_dict
+
 def load_lstm_data(data_dir, site, year):
     s = str(site)
     y = str(year)
@@ -10,8 +23,11 @@ def load_lstm_data(data_dir, site, year):
     obs_f_path = data_dir+'obs_'+s+'.txt' 
     forcing_f_path = data_dir+'forcing_'+s+'.txt' 
     with open(obs_f_path, 'r') as f:
-            obs = pd.read_csv(f, header=None, delimiter=r"\s+")
+        obs = pd.read_csv(f, header=None, delimiter=r"\s+")
+        obs.columns=['year','day','hour', 'x1', 'x2', 'lh', 'sm', 'p']
     with open(forcing_f_path, 'r') as f:
-            forcing = pd.read_csv(f, header=None, delimiter=r"\s+")
+        forcing = pd.read_csv(f, header=None, delimiter=r"\s+")
+        forcing.columns=['year','day','hour', 'wind', 'dummy', 'temp', 'humid', 'press', 'sw', 'lw', 'p']
     data_dict = {'obs':obs, 'forcing':forcing}
+    data_dict = split_test_train(data_dict, 2006)
     return data_dict
--- torch1.py
+++ torch1.py
@@ -2,38 +2,38 @@ import pandas as pd
 import numpy as np
 import pickle as pkl
 from load_lstm_data import load_lstm_data
-#import torch
-#import torch.nn as nn
-#import torch.nn.functional as F
-#import torch.optim as optim
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
 
-#torch.manual_seed(1)
+# Initialize stuff
+torch.manual_seed(1)
+hidden_state_size = 4
 
 #import the test and training data as pandas dataframes for now.
 data_dir = '/discover/nobackup/jframe/data/pals/full_data/'
 site = 2
-year = 2000
+year = 2006
 
 data_dict = load_lstm_data(data_dir, site, year)
-print(data_dict)
-exit()
 
-lstm = nn.LSTM(3, 3)  # Input dim is 3, output dim is 3
-inputs = [torch.randn(1, 3) for _ in range(10)]  # make a sequence of length 5
+inputShape = data_dict['forcing_train'].shape
+inputData = np.transpose(np.array(data_dict['forcing_train']))
 
-print(inputs)
-exit()
+lstm = nn.LSTM(hidden_state_size, hidden_state_size)
+inputs = [torch.tensor(list(inputData[0:hidden_state_size,i])) for i in range(inputShape[0])]
 
 # initialize the hidden state.
-hidden = (torch.randn(1, 1, 3),
-          torch.randn(1, 1, 3))
+hidden = (torch.randn(1, 1, hidden_state_size),
+          torch.randn(1, 1, hidden_state_size))
 
 print('doing the sequance in a loop.')
 
 for i in inputs:
-      # Step through the sequence one element at a time.
-      # after each step, hidden contains the hidden state.
-      out, hidden = lstm(i.view(1, 1, -1), hidden)
+    # Step through the sequence one element at a time.
+    # after each step, hidden contains the hidden state.
+    out, hidden = lstm(i.view(1, 1, -1), hidden)
 
 print('out')
 print(out)
@@ -52,7 +52,7 @@ print('doing the entire sequence all at once.')
 # by passing it as an argument  to the lstm at a later time
 # Add the extra 2nd dimension
 inputs = torch.cat(inputs).view(len(inputs), 1, -1)
-hidden = (torch.randn(1, 1, 3), torch.randn(1, 1, 3))  # clean out hidden state
+hidden = (torch.randn(1, 1, hidden_state_size), torch.randn(1, 1, hidden_state_size))  # clean out hidden state
 out, hidden = lstm(inputs, hidden)
 print('out')
 print(out)
