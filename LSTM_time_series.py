0cb665d26d8eb772ecd6b36e4097f51d2f05f32d
jmframe <jmframe@crimson.ua.edu>
2020-07-31 00:24:56 -0400

    MNIST example

--- LSTM_time_series.py
+++ LSTM_time_series.py
@@ -5,47 +5,75 @@ import matplotlib
 import numpy as np
 import pandas as pd
 import sklearn
-#import torch
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
 
 
 # Initialize stuff
-#torch.manual_seed(1)
-hidden_state_size = 4
+torch.manual_seed(1)
+hidden_state_size = 3
 
 #import the test and training data as pandas dataframes for now.
 data_dir = '/discover/nobackup/jframe/data/pals/full_data/'
 site = 2
-year = 2006
+year_test = 2005
+year_val = 2006
 
-df = load_lstm_data(data_dir, site, year)
+df = load_lstm_data(data_dir, site, year_test, year_val)
 
 from sklearn.preprocessing import StandardScaler
 
 scaler = StandardScaler()
 datas = ["forcing_train", "forcing_val", "forcing_test", "obs_train","obs_val","obs_test"] 
-train_forc = scaler.transform(df["forcing_train"])
+train_forc = scaler.fit_transform(df["forcing_train"])
+input_feature_size = train_forc.shape[1]
 val_forc = scaler.transform(df["forcing_val"])
 test_forc = scaler.transform(df["forcing_test"])
-train_obs = scaler.transform(df["obs_train"])
+train_obs = scaler.fit_transform(df["obs_train"])
 val_obs = scaler.transform(df["obs_val"])
 test_obs = scaler.transform(df["obs_test"])
 
-from torch.autograd import Variable
-def transform_data(arr1, arr2, seq_len):
-    x, y = [], []
-    for i in range(len(arr1) - seq_len):
-        x_i = arr1[i : i + seq_len]
-        y_i = arr2[i + 1 : i + seq_len + 1]
-        x.append(x_i)
-        y.append(y_i)
-    x_arr = np.array(x).reshape(-1, seq_len)
-    y_arr = np.array(y).reshape(-1, seq_len)
-    x_var = Variable(torch.from_numpy(x_arr).float())
-    y_var = Variable(torch.from_numpy(y_arr).float())
-    return x_var, y_var
 
-seq_len = 100
+lstm = nn.LSTM(input_feature_size, hidden_state_size)
+#inputs = [torch.tensor(list(train_forc[0:hidden_state_size,i])) for i in range(train_forc.shape[1])]
+inputs = [torch.tensor(list(np.array(df["forcing_train"])[i,:])) for i in range(train_forc.shape[0])]
 
-x_train, y_train = transform_data(train_arr, seq_len)
-x_val, y_val = transform_data(val_arr, seq_len)
-x_test, y_test = transform_data(test_arr, seq_len)
+# initialize the hidden state.
+hidden = (torch.randn(1, 1, hidden_state_size),
+          torch.randn(1, 1, hidden_state_size))
+
+#print('doing the sequance in a loop.')
+#
+#for i in inputs:
+#    # Step through the sequence one element at a time.
+#    # after each step, hidden contains the hidden state.
+#    out, hidden = lstm(i.view(1, 1, -1), hidden)
+#
+#print('out')
+#print(out)
+#print('hidden')
+#print(hidden)
+
+#print('doing the entire sequence all at once.')
+
+# alternatively, we can do the entire sequence all at once.
+# the first value returned by LSTM is all of the hidden states throughout
+# the sequence. the second is just the most recent hidden state
+# (compare the last slice of "out" with "hidden" below, they are the same)
+# The reason for this is that:
+# "out" will give you access to all hidden states in the sequence
+# "hidden" will allow you to continue the sequence and backpropagate,
+# by passing it as an argument  to the lstm at a later time
+# Add the extra 2nd dimension
+inputs = torch.cat(inputs).view(len(inputs), 1, -1)
+hidden = (torch.randn(1, 1, hidden_state_size), torch.randn(1, 1, hidden_state_size))  # clean out hidden state
+out, hidden = lstm(inputs, hidden)
+print('out')
+print(out)
+print('hidden')
+print(hidden)
+
+
+
--- load_lstm_data.py
+++ load_lstm_data.py
@@ -5,27 +5,25 @@ import pickle as pkl
 
 def split_test_train(data_dict, year_test, year_val):
     obs_drop = ['year','day','hour', 'x1', 'x2', 'p']
-    data_dict['obs_train'] = data_dict['obs'].loc[data_dict['obs']['year'] not in [year_test,year_val]]
-    data_dict['obs_test'] = data_dict['obs'].loc[data_dict['obs']['year'] = year_test]
-    data_dict['obs_val'] = data_dict['obs'].loc[data_dict['obs']['year'] = year_val]
+    data_dict['obs_train'] = data_dict['obs'][~data_dict['obs']['year'].isin([year_test,year_val])]
+    data_dict['obs_test'] = data_dict['obs'].loc[data_dict['obs']['year'] == year_test]
+    data_dict['obs_val'] = data_dict['obs'].loc[data_dict['obs']['year'] == year_val]
     data_dict['obs_train'] = data_dict['obs_train'].drop(obs_drop, axis=1)
     data_dict['obs_test'] = data_dict['obs_test'].drop(obs_drop, axis=1)
     data_dict['obs_val'] = data_dict['obs_val'].drop(obs_drop, axis=1)
 
     forcing_drop = ['year','day','hour', 'dummy']
-    data_dict['forcing_train'] = data_dict['forcing'].loc[data_dict['forcing']['year'] not in [year_test,year_val]]
-    data_dict['forcing_test'] = data_dict['forcing'].loc[data_dict['forcing']['year'] = year_test]
-    data_dict['forcing_val'] = data_dict['forcing'].loc[data_dict['forcing']['year'] = year_val]
-    data_dict['forcing_train'] = data_dict['forcing_train'].drop(obs_drop, axis=1)
-    data_dict['forcing_test'] = data_dict['forcing_test'].drop(obs_drop, axis=1)
-    data_dict['forcing_val'] = data_dict['forcing_val'].drop(obs_drop, axis=1)
+    data_dict['forcing_train'] = data_dict['forcing'][~data_dict['forcing']['year'].isin([year_test,year_val])]
+    data_dict['forcing_test'] = data_dict['forcing'].loc[data_dict['forcing']['year'] == year_test]
+    data_dict['forcing_val'] = data_dict['forcing'].loc[data_dict['forcing']['year'] == year_val]
+    data_dict['forcing_train'] = data_dict['forcing_train'].drop(forcing_drop, axis=1)
+    data_dict['forcing_test'] = data_dict['forcing_test'].drop(forcing_drop, axis=1)
+    data_dict['forcing_val'] = data_dict['forcing_val'].drop(forcing_drop, axis=1)
 
     return data_dict
 
-def load_lstm_data(data_dir, site, year):
+def load_lstm_data(data_dir, site, year_test, year_val):
     s = str(site)
-    y = str(year)
-    siteyear = s+'_'+y
     obs_f_path = data_dir+'obs_'+s+'.txt' 
     forcing_f_path = data_dir+'forcing_'+s+'.txt' 
     with open(obs_f_path, 'r') as f:
@@ -35,5 +33,5 @@ def load_lstm_data(data_dir, site, year):
         forcing = pd.read_csv(f, header=None, delimiter=r"\s+")
         forcing.columns=['year','day','hour', 'wind', 'dummy', 'temp', 'humid', 'press', 'sw', 'lw', 'p']
     data_dict = {'obs':obs, 'forcing':forcing}
-    data_dict = split_test_train(data_dict, 2005, 2006)
+    data_dict = split_test_train(data_dict, year_test, year_val)
     return data_dict
